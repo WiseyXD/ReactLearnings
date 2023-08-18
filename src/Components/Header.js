ae9312abe7a010de8fb1ac92f7be88e6503b369b
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-08-18 20:39:49 +0530

    UI : Header Done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,84 +22,89 @@ export default function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	// Header Done
 	return (
-		<>
-			<div id="Header" className="mx-4 flex justify-between items-center">
-				<a href="/">
-					<img src={UseState} alt="Logo" className="w-[30%]" />
-				</a>
-				<div className="navbar hidden lg:block">
-					<ul className="flex">
-						<Link to="/">
-							<li key={1}>Home</li>
-						</Link>
-						<Link to="/about">
-							<li key={2}>About Us</li>
-						</Link>
-						<Link to="/contact">
-							<li key={3}>Contact Us</li>
-						</Link>
+		<div className="bg-gray-200 shadow-lg shadow-slate-300 mb-5">
+			<div className="max-w-7xl w-[90%] mx-auto my-0 ">
+				<div
+					id="Header"
+					className="mx-4 flex justify-between items-center basis-1/3 "
+				>
+					<a href="/">
+						<img src={UseState} alt="Logo" className="w-[30%]" />
+					</a>
+					<div className="navbar hidden lg:block basis-2/3">
+						<ul className="flex gap-x-4 text-lg ">
+							<Link to="/">
+								<li key={1}>Home</li>
+							</Link>
+							<Link to="/about">
+								<li key={2}>About Us</li>
+							</Link>
+							<Link to="/contact">
+								<li key={3}>Contact Us</li>
+							</Link>
 
-						<Link to="/instamart">
-							<li key={6}>Instamart</li>
-						</Link>
-						<Link to="#" onClick={() => setLoggedIn(!loggedIn)}>
-							<li key={7}>{loggedIn ? "Logout" : "Login"}</li>
-						</Link>
-						<Link to="/cart">
-							<li key={4}>Cart - {cartItems.length} items</li>
-						</Link>
-						<Link to="#">
-							<li key={5}>{isOnline ? "✅" : "🔴"}</li>
-						</Link>
-					</ul>
+							<Link to="/instamart">
+								<li key={6}>Instamart</li>
+							</Link>
+							<Link to="#" onClick={() => setLoggedIn(!loggedIn)}>
+								<li key={7}>{loggedIn ? "Logout" : "Login"}</li>
+							</Link>
+							<Link to="/cart">
+								<li key={4}>Cart - {cartItems.length} items</li>
+							</Link>
+							<Link to="#">
+								<li key={5}>{isOnline ? "✅" : "🔴"}</li>
+							</Link>
+						</ul>
+					</div>
+					<>
+						{isMenuOpen ? (
+							<button className="lg:hidden">
+								<RxCross1
+									size={30}
+									onClick={() => setIsMenuOpen(!isMenuOpen)}
+								/>
+							</button>
+						) : (
+							<button className="lg:hidden">
+								<RxHamburgerMenu
+									size={30}
+									onClick={() => setIsMenuOpen(!isMenuOpen)}
+								/>
+							</button>
+						)}
+					</>
 				</div>
-				<>
-					{isMenuOpen ? (
-						<button className="lg:hidden">
-							<RxCross1
-								size={30}
-								onClick={() => setIsMenuOpen(!isMenuOpen)}
-							/>
-						</button>
-					) : (
-						<button className="lg:hidden">
-							<RxHamburgerMenu
-								size={30}
-								onClick={() => setIsMenuOpen(!isMenuOpen)}
-							/>
-						</button>
+
+				<div className="absolute bg-gray-100 w-full ">
+					{isMenuOpen && (
+						<ul className="flex flex-col justify-center items-center gap-y-1">
+							<Link to="/">
+								<li key={1}>Home</li>
+							</Link>
+							<Link to="/about">
+								<li key={2}>About Us</li>
+							</Link>
+							<Link to="/contact">
+								<li key={3}>Contact Us</li>
+							</Link>
+
+							<Link to="/instamart">
+								<li key={6}>Instamart</li>
+							</Link>
+							<Link to="#" onClick={() => setLoggedIn(!loggedIn)}>
+								<li key={7}>{loggedIn ? "Logout" : "Login"}</li>
+							</Link>
+							<Link to="/cart">
+								<li key={4}>Cart - {cartItems.length} items</li>
+							</Link>
+							<Link to="#">
+								<li key={5}>{isOnline ? "✅" : "🔴"}</li>
+							</Link>
+						</ul>
 					)}
-				</>
+				</div>
 			</div>
-
-			<div className="absolute bg-gray-100 w-full ">
-				{isMenuOpen && (
-					<ul className="flex flex-col justify-center items-center gap-y-1">
-						<Link to="/">
-							<li key={1}>Home</li>
-						</Link>
-						<Link to="/about">
-							<li key={2}>About Us</li>
-						</Link>
-						<Link to="/contact">
-							<li key={3}>Contact Us</li>
-						</Link>
-
-						<Link to="/instamart">
-							<li key={6}>Instamart</li>
-						</Link>
-						<Link to="#" onClick={() => setLoggedIn(!loggedIn)}>
-							<li key={7}>{loggedIn ? "Logout" : "Login"}</li>
-						</Link>
-						<Link to="/cart">
-							<li key={4}>Cart - {cartItems.length} items</li>
-						</Link>
-						<Link to="#">
-							<li key={5}>{isOnline ? "✅" : "🔴"}</li>
-						</Link>
-					</ul>
-				)}
-			</div>
-		</>
+		</div>
 	);
 }
