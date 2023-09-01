b02aca67739af0c9077dc08a8e8096b796c6f643
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-01 20:16:43 +0530

    UI : Restaurant Details Page Done

--- src/Components/RestaurantDetails.js
+++ src/Components/RestaurantDetails.js
@@ -4,6 +4,7 @@ import { MENU_URL, cloudinarImgUrl } from "../config";
 import Shimmer from "./Shimmer";
 import { useDispatch } from "react-redux";
 import { addItem } from "../utils/cartSlice";
+import { AiFillStar } from "react-icons/ai";
 
 export default function RestaurantDetails() {
 	const { id } = useParams();
@@ -32,32 +33,44 @@ export default function RestaurantDetails() {
 		<Shimmer />
 	) : (
 		<div className="max-w-7xl w-[90%] mx-auto my-0 flex flex-col justify-center items-center">
-			<div className="card">
+			<div className="">
 				<img
 					src={cloudinarImgUrl + restaurant.cloudinaryImageId}
 					alt=""
 				/>
 				<div className="">
-					<div className="flex justify-between items-center">
+					<div className="flex justify-between item">
 						<h1 className="text-2xl font-semibold ">
 							{restaurant.name}
 						</h1>
-						<h3>{restaurant.avgRating} Stars</h3>
+						<div className="flex justify-center items-center text-lg gap-1">
+							{restaurant.avgRating} <AiFillStar />
+						</div>
 					</div>
-					<h3>{restaurant.costForTwoMessage}</h3>
-					<h3>{restaurant.cuisines.join(", ")}</h3>
-					<h5>Restaurant ID : {id}</h5>
+					<h3 className="text-lg">{restaurant.costForTwoMessage}</h3>
+					<h3 className="text-lg">{restaurant.cuisines}</h3>
+					{/* <h5>Restaurant ID : {id}</h5> */}
 				</div>
 			</div>
 			{menu === undefined ? (
 				<h1>Menu Not Available</h1>
 			) : (
-				<div className="menuList">
-					<h3>Recommended</h3>
+				<div className="">
+					<h3 className="font-medium text-xl">
+						Recommended - {menu.length} items
+					</h3>
 					{menu.map((ele) => (
-						<li key={ele.card.info.id}>
-							{ele.card.info.name}{" "}
-							<button onClick={() => handleAddItem(ele)}>
+						<li
+							key={ele.card.info.id}
+							className="list-none flex justify-between py-2 my-2 bg-gray-200 px-2 rounded-md"
+						>
+							<h3 className="text-lg font-normal">
+								{ele.card.info.name}{" "}
+							</h3>
+							<button
+								className="bg-green-400 text-gray-100 px-2 py-1 rounded-md hover:shadow-lg duration-200 ease-in-out"
+								onClick={() => handleAddItem(ele)}
+							>
 								Add Item
 							</button>
 						</li>
