635e82d4091c987645307fbd3787fdc9bb96683f
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-01 19:37:23 +0530

    UI : Layout for the Restaurant Detail Page Done

--- src/Components/RestaurantDetails.js
+++ src/Components/RestaurantDetails.js
@@ -31,17 +31,23 @@ export default function RestaurantDetails() {
 	return restaurant === null ? (
 		<Shimmer />
 	) : (
-		<div className="container">
+		<div className="max-w-7xl w-[90%] mx-auto my-0 flex flex-col justify-center items-center">
 			<div className="card">
 				<img
 					src={cloudinarImgUrl + restaurant.cloudinaryImageId}
 					alt=""
 				/>
-				<h1>{restaurant.name}</h1>
-				<h3>{restaurant.avgRating} Stars</h3>
-				<h3>{restaurant.costForTwoMessage}</h3>
-				<h3>{restaurant.cuisines}</h3>
-				<h5>Restaurant ID : {id}</h5>
+				<div className="">
+					<div className="flex justify-between items-center">
+						<h1 className="text-2xl font-semibold ">
+							{restaurant.name}
+						</h1>
+						<h3>{restaurant.avgRating} Stars</h3>
+					</div>
+					<h3>{restaurant.costForTwoMessage}</h3>
+					<h3>{restaurant.cuisines.join(", ")}</h3>
+					<h5>Restaurant ID : {id}</h5>
+				</div>
 			</div>
 			{menu === undefined ? (
 				<h1>Menu Not Available</h1>
