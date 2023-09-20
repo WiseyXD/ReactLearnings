61bbc2c8660b5949cc6fc7d318f2e7902f036df1
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-20 09:15:38 +0530

    feat : Firebase Added Successfully

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { app } from "../firebase";
+import { getDatabase, ref, set } from "firebase/database";
 
+const db = getDatabase(app);
 export default function Profile() {
+	function updateDB() {
+		set(ref(db, "users/ari"), {
+			name: "Aryan",
+			age: 20,
+			workout: true,
+		});
+	}
 	return (
 		<div>
-			<h2>HEllo There</h2>
+			<button
+				onClick={updateDB}
+				className="cursor-pointer text-3xl font-bold"
+			>
+				Click me
+			</button>
 		</div>
 	);
 }
