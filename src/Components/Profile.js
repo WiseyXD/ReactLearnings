c77f98eff0340294d73273eb79f8e3c7f6b822c7
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-20 22:24:17 +0530

    Done with Google Signin

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,73 +2,9 @@ import React, { useState } from "react";
 import { useFirebase } from "../utils/firebaseContext";
 
 export default function Profile() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [loginEmail, setLoginEmail] = useState("");
-	const [loginPassword, setLoginPassword] = useState("");
-	const firebase = useFirebase();
-	console.log(firebase);
-
-	const createUser = () => {
-		firebase
-			.signUpUser(email, password)
-			.then(alert("Signed in successfull"))
-			.catch((err) => console.log(err));
-	};
-	const Gsignin = () => {
-		firebase
-			.googleSignin()
-			.then(console.log("Success Login"))
-			.catch((err) => console.log(err));
-	};
 	return (
 		<>
-			<div>
-				<h1 className="text-2xl font-semibold">Register </h1>
-				<input
-					type="email"
-					placeholder="email"
-					name="email"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					name="password"
-					id=""
-					placeholder="password"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-
-				<button
-					onClick={createUser}
-					className="cursor-pointer text-3xl font-bold"
-				>
-					SignUp
-				</button>
-			</div>
-			<div>
-				<h1 className="text-2xl font-semibold">Login </h1>
-				<input
-					type="email"
-					placeholder="email"
-					name="email"
-					onChange={(e) => setLoginEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					name="password"
-					id=""
-					placeholder="password"
-					onChange={(e) => setLoginPassword(e.target.value)}
-				/>
-
-				<button
-					onClick={Gsignin}
-					className="cursor-pointer text-3xl font-bold"
-				>
-					Login
-				</button>
-			</div>
+			<h1>Profile</h1>
 		</>
 	);
 }
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import { useFirebase } from "../utils/firebaseContext";
 
 export default function Signup() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [loginEmail, setLoginEmail] = useState("");
+	const [loginPassword, setLoginPassword] = useState("");
+
 	const firebase = useFirebase();
 	console.log(firebase);
+
+	const createUser = () => {
+		firebase
+			.signUpUser(email, password)
+			.then(alert("Signed in successfull"))
+			.catch((err) => console.log(err));
+	};
 	const GsignUp = () => {
 		firebase
 			.googleSignin()
@@ -11,6 +24,32 @@ export default function Signup() {
 	};
 	return (
 		<div className="container w-3/4 mx-auto my-0">
+			<div>
+				<h1 className="text-2xl font-semibold">Register </h1>
+				<input
+					type="email"
+					placeholder="email"
+					name="email"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					name="password"
+					id=""
+					placeholder="password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<button
+					onClick={createUser}
+					className="cursor-pointer text-3xl font-bold"
+				>
+					SignUp
+				</button>
+			</div>
+			<div>
+				<h1 className="text-2xl m-5"> OR</h1>
+			</div>
 			<button
 				className="text-3xl font-semibold bg-green-500"
 				onClick={GsignUp}
